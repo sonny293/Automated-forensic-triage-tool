1a313c6cbeba9c346fc64de220fdee97bfde8e9e
Sonny <sonnybowers57@gmail.com>
2026-02-22 15:58:41 +0000

    fix wevtutil 1

--- collector/event_logs.py
+++ collector/event_logs.py
@@ -4,15 +4,23 @@ collects windows events logs for to later parse (Event ID 4625)
 """
 
 import subprocess
+from pathlib import Path
 
 #Runs shell command to find current directory and save it to varieble
+"""
 Result = subprocess.run(['pwd'], capture_output=True, text=True, shell=True)
 ExportLocation = Result.stdout.strip()
-
-ExportLocation= (f'{ExportLocation}/security.evtx')
+"""
+# collects current directory and joins it to file name for export location
+CurrentDir = Path.cwd()
+path = CurrentDir / "security.evtx"
 
 # run 'wevtutil' shell command with current location to export security logs
-subprocess.run(f'wevtutil', 'epl', 'Security', '{ExportLocation}'), check=True
+subprocess.run(
+    ["wevtutil", "epl", "Security", str(path)],
+    check=True
+)
+
 
 
 if __name__ == "__main__":
