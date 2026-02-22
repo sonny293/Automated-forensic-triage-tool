291fa32ab22f3d758bcf70e9487fca53fa9408d2
Sonny <sonnybowers57@gmail.com>
2026-02-22 15:07:54 +0000

    added event log export

---
+++ collector/event_logs.py
@@ -0,0 +1,17 @@
+"""
+Docstring for collector.event_logs
+collects windows events logs for to later parse (Event ID 4625) 
+"""
+
+import subprocess
+
+#Runs shell command to find current directory and save it to varieble
+Result = subprocess.run(['pwd'], capture_output=True, text=True, shell=True)
+ExportLocation = Result.stdout.strip()
+
+# run 'wevtutil' shell command with current location to export security logs
+subprocess.run(f'wevtutil epl Security {ExportLocation}/security.evtx')
+
+
+if __name__ == "__main__":
+    print('hi')
