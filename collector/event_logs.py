5c26355cbc6ea53b2059168e7a18bf879c6fd9f0
Sonny <sonnybowers57@gmail.com>
2026-02-22 15:30:39 +0000

    collector edit 1 'wevtutil'

--- collector/event_logs.py
+++ collector/event_logs.py
@@ -9,8 +9,10 @@ import subprocess
 Result = subprocess.run(['pwd'], capture_output=True, text=True, shell=True)
 ExportLocation = Result.stdout.strip()
 
+ExportLocation= (f'{ExportLocation}/security.evtx')
+
 # run 'wevtutil' shell command with current location to export security logs
-subprocess.run(f'wevtutil epl Security {ExportLocation}/security.evtx')
+subprocess.run(f'wevtutil', 'epl', 'Security', '{ExportLocation}'), check=True
 
 
 if __name__ == "__main__":
