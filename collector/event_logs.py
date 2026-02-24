294c72e6fe6d4d5ef42539889d91d04c5193057d
Sonny <sonnybowers57@gmail.com>
2026-02-24 16:13:01 +0000

    updated event_parse with logging, faster runtime, etc

--- collector/event_logs.py
+++ collector/event_logs.py
@@ -6,16 +6,6 @@ collects windows events logs for to later parse (Event ID 4625)
 import subprocess
 from pathlib import Path
 
-#Runs shell command to find current directory and save it to varieble
-"""
-Result = subprocess.run(['pwd'], capture_output=True, text=True, shell=True)
-ExportLocation = Result.stdout.strip()
-"""
-# collects current directory and joins it to file name for export location
-"""
-change to appropriate location
-"""
-
 def file_path():
     base_dir = Path("Logs/security.evtx")
     path = base_dir.resolve()
--- parsers/event_parse.py
+++ parsers/event_parse.py
@@ -9,6 +9,18 @@ import json
 import os
 from pathlib import Path
 import time
+import logging
+from datetime import datetime
+import sys
+from tqdm import tqdm 
+
+#loggin
+
+logging.basicConfig(
+    level=logging.DEBUG,  # minimum level to show
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+logging.getLogger("Evtx").setLevel(logging.WARNING)
 
 def file_path():
     base_dir = Path(__file__).resolve().parent
@@ -19,38 +31,53 @@ path = file_path()
 #path = r"/Users/sonnybowers/Documents/University/Group Project/AFTT/main/Automated-forensic-triage-tool/collector/Logs/security.evtx"
 
 def main(path):
+    logging.debug("Loading Windows Security Event Log")
+    Artifact_count=0  
+    Failed_count=0
     with Evtx(path) as log, open("Security_failed_logins.json", "w", encoding="utf-8") as f:
-        count=0
+        logging.debug("Converting Event Log Into JSON Dictionay\n")   
         for record in log.records():
+            print(f"\rArtifacts Processed: {Artifact_count}", end="", flush=True)
+            #logging.info(f"Artifact:{Artifact_count} Processing")
+            Artifact_count+=1
             xml_str = record.xml()
+
             # Convert XML string to Python dict
             event_dict = xmltodict.parse(xml_str)
             #filter the dictionary for needed fields
+            # Save each events that matched event id 4625 as JSON in new line
 
-            for event in event_dict :
+            event_id = event_dict["Event"]["System"]["EventID"]["#text"]
+            
+            if event_id == "4625":
                 filtered = {
                 "EventID": event_dict["Event"]["System"]["EventID"]["#text"],
                 "TimeCreated": event_dict["Event"]["System"]["TimeCreated"]["@SystemTime"],
-                #"EventData": event_dict["Event"]["EventData"]["Data"][1].get["#text"],
                 }
-            # Save each events that matched event id 4625 as JSON in new line
-            event_id = event_dict["Event"]["System"]["EventID"]["#text"]
-            if event_id == "4625":
-                print('Failed Login Attempt:')
+
                 f.write(json.dumps(filtered) + "\n")
-                print(filtered)
-                count+=1          
-    print(f"Artifacts Found:{count}")
-    return event_dict
+                logging.debug(f"Failed Loggin Attempt:\n{filtered}")
+
+                data_list = event_dict["Event"]["EventData"]["Data"]
+
+                if data_list != None:
+                    print("Account:" + data_list[5]["#text"] + "\n")
+
+                Failed_count+=1        
+
+    print(f"\n\nTotal Artifacts Found:{Artifact_count}")
+    print(f"Failed Loggins Found:{Failed_count}")
 
 
 
 if __name__ == "__main__":
     print('')
-    event_dict = main(path)
-    data_list = event_dict["Event"]["EventData"]["Data"]
-    for event in data_list:
-        print(data_list[1]["#text"])
+    start = time.time()
+    main(path)
+    end = time.time()
+    print(f"Time Taken: {round(end - start, 3)} sec")
+ 
+
    
 
 
