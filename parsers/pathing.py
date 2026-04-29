a1452f599e7c6df72c639138721845a7952964f5
sonny293 <sonnybowers57@gmail.com>
2026-04-29 20:35:06 +0100

    extra returns

--- parsers/pathing.py
+++ parsers/pathing.py
@@ -25,7 +25,7 @@ def path_find():
     attempts_output_path = os.path.join(tool_dir, "results/event_logs", "failed_attempts.json")
 
     #returns variouse paths for other scripts to use
-    return evtx_path, report_output_path, attempts_output_path, browser_path, browser_output_path
+    return evtx_path, report_output_path, attempts_output_path
 
 def brow_path_find():
     base_dir = Path(__file__).resolve().parent
@@ -34,7 +34,7 @@ def brow_path_find():
     #finds current directory
     tool_dir = os.path.dirname(os.path.abspath(__file__))
     tool_dir = os.path.dirname(tool_dir)  # go up one level
-    
+
     browser_output_path = os.path.join(tool_dir, "results/browser_history", "filtered_history.json")
 
     return browser_path, browser_output_path
