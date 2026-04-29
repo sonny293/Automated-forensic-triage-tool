e634ccad6a06698cc1920710fc6397ffbac57269
sonny293 <sonnybowers57@gmail.com>
2026-04-29 20:31:19 +0100

    pathing error

--- parsers/pathing.py
+++ parsers/pathing.py
@@ -30,6 +30,11 @@ def path_find():
 def brow_path_find():
     base_dir = Path(__file__).resolve().parent
     browser_path = base_dir.parent / "collector" / "Browser_Hist" / "History.json"
+
+    #finds current directory
+    tool_dir = os.path.dirname(os.path.abspath(__file__))
+    tool_dir = os.path.dirname(tool_dir)  # go up one level
+    
     browser_output_path = os.path.join(tool_dir, "results/browser_history", "filtered_history.json")
 
     return browser_path, browser_output_path
